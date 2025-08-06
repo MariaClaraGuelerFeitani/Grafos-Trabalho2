5818cbf101a81e56989531da7211a6f2ff0f45a8
Maria Clara Gueler Feitani <mariaclaraguelerfeitani@gmail.com>
2025-08-06 20:12:05 -0300

    Delete t2 directory

--- t2/grafo.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAFO_H_INCLUDED
-#define GRAFO_H_INCLUDED
-
-void GrafoMatriz(int num, int mat[num][num]);
-void adicionarLigacao(int num, int mat[num][num], int v1, int v2, int custo);
-void removerLigacao(int num, int mat[num][num], int proibido);
-void imprimirGrafo(int num, int mat[num][num]);
-
-#endif // GRAFO_H_INCLUDED
