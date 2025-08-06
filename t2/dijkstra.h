2719b51e935dfd58164e13069d5ff64f33f04d30
Maria Clara Gueler Feitani <mariaclaraguelerfeitani@gmail.com>
2025-08-06 20:07:11 -0300

    Add files via upload

---
+++ t2/dijkstra.h
@@ -0,0 +1,7 @@
+#ifndef DIJKSTRA_H_INCLUDED
+#define DIJKSTRA_H_INCLUDED
+
+void Dijkstra(int num, int mat[num][num], int origem, int distancia[], int predecessores[]);
+
+
+#endif // DIJKSTRA_H_INCLUDED
