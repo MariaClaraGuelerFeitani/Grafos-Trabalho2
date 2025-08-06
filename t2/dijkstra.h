278e247171da7669bcaf010eaf1ce5c4be83926b
Maria Clara Gueler Feitani <mariaclaraguelerfeitani@gmail.com>
2025-08-06 20:07:39 -0300

    Delete t2 directory

--- t2/dijkstra.h
+++
@@ -1,7 +0,0 @@
-#ifndef DIJKSTRA_H_INCLUDED
-#define DIJKSTRA_H_INCLUDED
-
-void Dijkstra(int num, int mat[num][num], int origem, int distancia[], int predecessores[]);
-
-
-#endif // DIJKSTRA_H_INCLUDED
